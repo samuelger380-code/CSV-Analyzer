5d29f0e064644c9c2dad72863250d11463492544
Samuel Gervais <samuelgervais@Samuels-MacAir.local>
2026-05-01 00:52:08 -0400

    read CSV headers and row count

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Argparse lets your tool work across multiple files
 from pathlib import Path 
 import argparse
+import csv
 
 def main(): 
     parser = argparse.ArgumentParser(description = "Analyze a CSV file")
@@ -12,6 +13,16 @@ def main():
         print(f"Error: file '{args.file}' not found ")
         return
     print (f"File found: {path}")
+
+    with open(path, newline="") as f:
+        reader = csv.reader(f)
+        headers = next(reader)
+        row_count = sum(1 for row in reader)
+
+    print(f"Columns: {headers}")
+    print(f"Rows: {row_count}")
+
+
 
 if __name__ == "__main__":
     main()
